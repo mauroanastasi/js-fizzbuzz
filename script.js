c4a464c1089dfee217777a923c413aa7263c1d62
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-19 09:23:05 +0100

    [FIX] modifico il multiplo in comune in un if a parte

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ for( let i=1; i<=n; i++){
     else if(i % 5 == 0){
         console.log(msgCinque)
     }    
-
-    else if((i % 3 == 0)&&(i % 5 == 0)){
-        console.log(msgTreCinque)
-    } 
     
     else{
         console.log(i)
     }
+
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(msgTreCinque)
+    } 
+
 }
