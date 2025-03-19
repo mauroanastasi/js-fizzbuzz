a708cf89a0738aa4b67863e2fc696a27ad7ce997
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-19 08:53:52 +0100

    [UPD] primo multiplo

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // inizializzo variabili
 
 let n = 100
-let msgTre = Fizz
-let msgCinque = Buzz
-let msgTreCinque = FizzBuzz
+let msgTre = `Fizz`
+let msgCinque = `Buzz`
+let msgTreCinque = `FizzBuzz`
 
 // corpo del programma
 
+for( let i=1; i<=n; i++){
+    
+    if(i % 3 == 0){
+        console.log(msgTre)
+    }
+
+
+
+
+    
+}
